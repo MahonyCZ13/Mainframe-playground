5e2e2f8ce787a67f4efb3831c1e8a113a5e58470
MahonyCZ13 <maronek.petr@outlook.com>
2024-04-02 14:08:47 +0200

    added line numbers for completenes

--- cobol/hello/hello.cob
+++ cobol/hello/hello.cob
@@ -1,12 +1,12 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. HELLO.
-       AUTHOR. PETR.
-       DATE-WRITTEN 25-03-2024.
+000010 IDENTIFICATION DIVISION.
+000020 PROGRAM-ID. HELLO.
+000030 AUTHOR. PETR.
+000040 DATE-WRITTEN 25-03-2024.
       * The author and date written are done via comments in modern environments. 
-       PROCEDURE DIVISION.
-           DISPLAY "Hello, world!".
-           STOP RUN.
-       END PROGRAM HELLO.
+000050 PROCEDURE DIVISION.
+000060     DISPLAY "Hello, world!".
+000070     STOP RUN.
+000080 END PROGRAM HELLO.
       * We don't need to use ALL CAPS. GnuCobol is fine with lower caps
       * From column 8 onwards, there are two section: A and B.
       * Section A is in columns 8-11 and it is reserved for devisions, sections and paragraphs.
